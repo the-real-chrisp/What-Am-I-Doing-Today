3e5943a19dd462a168ea36bf67a49b5bf2ec9761
Chris Purcell <chrisp0000@gmail.com>
2023-10-31 19:52:12 -0800

    Removed 1000 from append function

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ $(function () {
 
   const currentDayEl = $('#currentDay')
 
-  $(currentDayEl).append(formattedCurrentDate, 1000);
+  $(currentDayEl).append(formattedCurrentDate);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
